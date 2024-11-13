9e632570c0a3ef8265ece2a0c59102f3470ec139
GalD101 <galdal10@gmail.com>
2024-11-13 22:57:22 +0200

    added comments and indentation

--- exercise_1/main.s
+++ exercise_1/main.s
@@ -4,37 +4,35 @@
 .extern  rand
 
 ########################
+
 .section		.rodata
 
-    # yes and no characters to determine flow
-    yes_chr:    .byte 'y'
-    no_chr:     .byte 'n'
+# yes and no characters to determine flow
+yes_chr:    .byte 'y'
+no_chr:     .byte 'n'
 
-    # prompt messages asking for user input
-    usr_cnfg_seed_prmpt:	.string "Enter configuration seed: "
-    scanf_fmt_seed:         .string "%d"
+# prompt messages asking for user input
+usr_cnfg_seed_prmpt:	.string "Enter configuration seed: "
+scanf_fmt_seed:         .string "%d"
 
-    usr_easy_mode_prmpt:    .string "Would you like to play in easy mode? (y/n) "
-    scanf_fmt_yes_or_no:    .string "%c"
+usr_easy_mode_prmpt:    .string "Would you like to play in easy mode? (y/n) "
+scanf_fmt_yes_or_no:    .string "%c"
 
-    usr_guess_prmpt:	    .string "What is your guess? "
-    scanf_fmt_guess:        .string "%d"
+usr_guess_prmpt:	    .string "What is your guess? "
+scanf_fmt_guess:        .string "%d"
 
-    # messages regarding the state of the game
-    correct:			.string "Congratz! You won %u rounds!\n"
-    incorrect:		    .string "Incorrect. "
-    game_over:		    .string "Game over, you lost :(. The correct answer was %u\n"
-
-########################
-
+# messages regarding the state of the game
+correct:			.string "Congratz! You won %u rounds!\n"
+incorrect:		    .string "Incorrect. "
+game_over:		    .string "Game over, you lost :(. The correct answer was %u\n"
 
 ########################
 .section .data
 
-    # create memory for the user input
-    usr_input_seed: .int 0x00
-    usr_input_guess: .int 8, 0x00
-    rnd_num_generated: .long 0x00
+# create memory for the user input
+usr_input_seed: .int 0x00
+usr_input_guess: .int 8, 0x00
+rnd_num_generated: .long 0x00
 ########################
 
 
@@ -54,49 +52,72 @@
 .global     main
 .type main, @function
 main:
-    # boiler-plate code start
-    pushq	%rbp		#save the old frame pointer
-	movq	%rsp,	%rbp	#create the new frame pointer
-    # boiler-plate code end
+# boiler-plate code (copied from the examples in the exercise)
+pushq	%rbp                            # save the old frame pointer
+movq	%rsp,	%rbp	                # create the new frame pointer
 
-        # Print the prompt asking for the seed
-    movq    $usr_cnfg_seed_prmpt, %rdi
-    movq    $0, %rax
-    call    printf
 
-    # Scan the seed from the user
-    mov     $scanf_fmt_seed, %rdi           # Ensure this is "%d" for scanf
-    lea     usr_input_seed(%rip), %rsi
-    call    scanf
+# Print the prompt asking for the seed
+movq    $usr_cnfg_seed_prmpt, %rdi
+movq    $0, %rax
+call    printf
 
-    # Call srand with the seed
-    movq    usr_input_seed(%rip), %rdi
-    movq    $0, %rax
+# Scan the seed from the user
+mov     $scanf_fmt_seed, %rdi           # set the format as the first input for scanf
+lea     usr_input_seed(%rip), %rsi      # set the address of usr_input_seed as the second input for scanf (i.e. scanf("%d", &usr_input_seed))
+call    scanf
+
+# Call srand with the seed
+    movq    usr_input_seed(%rip), %rdi      # set the address of the usr_input_seed as the first (and only) argument for srand (passing by address)
+movq    $0, %rax                        # clear the rax register (it is customary to do this before every function call)
     call    srand
 
-    # Call rand and store its return value
-    movq    $0, %rdi                        # Not needed for rand, but safe
-    movq    $0, %rax
-    call    rand
-    movq    %rax, %rdx                      # Store rand result in rdx for division
-    
-    # Modulo the result by 11
-    xorq    %rdx, %rdx                      # Clear rdx before division
-    movq    $10, %rcx                       # Set divisor to 10
-    div     %rcx                            # rax = quotient, rdx = remainder
-    inc     %rdx
-    movq    %rdx, rnd_num_generated(%rip)   # Store remainder in rnd_num_generated
+# Call rand
+    movq    $0, %rdi                        # zero the rdi register (WHY?????)
+    movq    $0, %rax                        # clear the rax register (it is customary to do this before every function call)
+call    rand                            # the result from rand is now stored at rax (so it is also in eax (same register smaller portion of it)
 
-    # Print the random value (mod result)
-    movq    $scanf_fmt_seed, %rdi               # printf format string like "%d\n"
-    movq    rnd_num_generated(%rip), %rsi   # Load the result to be printed
-    movq    $0, %rax                        # Clear rax for variadic function
-    call    printf
+# Modulo the result from rand (which is stored in rax) by 10 and add 1
+        movq    $0, %rdx                        # clear rdx
+        movq    $10, %rcx
+        div     %rcx                            # rax = quotient, rdx = remainder. this is just how div works - not intuitive at all :(
+            inc     %rdx                            # add 1 to match the examples in the assignment instructions
+            movq    %rdx, rnd_num_generated(%rip)   # Store remainder in rnd_num_generated
+
+# Print easy mode prompt
+#TODO       
 
 
-    
-	
-	movq	$0, %rax	#return value is zero (just like in c - we tell the OS that this program finished seccessfully)
-	movq	%rbp, %rsp	#restore the old stack pointer - release all used memory.
-	popq	%rbp		#restore old frame pointer (the caller function frame)
-	ret			#return to caller function (OS)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Print the random value (mod result) (testing)
+        movq    $scanf_fmt_seed, %rdi           # printf format string like "%d\n"
+        movq    rnd_num_generated(%rip), %rsi   # Load the result to be printed
+        movq    $0, %rax                        # Clear rax for variadic function
+        call    printf
+
+
+
+
+movq	$0, %rax	                    # return value is zero (just like in c - we tell the OS that this program finished seccessfully)
+    movq	%rbp, %rsp	                # restore the old stack pointer - release all used memory.
+    popq	%rbp		                # restore old frame pointer (the caller function frame)
+ret			                            # return to caller function (OS)
