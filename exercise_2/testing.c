345889a6bca912924c00f26f45b5a668cf64a6fa
GalD101 <galdali10@gmail.com>
2024-11-30 13:33:41 +0200

    implemented pstrijcpy, but needs testing and some improvements

--- exercise_2/testing.c
+++ exercise_2/testing.c
@@ -8,17 +8,28 @@ typedef struct {
 
 // Declare the swapCase function
 extern void swapCase(Pstring* pstr);
-char pstrlen(Pstring* pstr);
+extern char pstrlen(Pstring* pstr);
+extern Pstring* pstrijcpy(Pstring* dst, Pstring* src, char i, char j);
+
 
 int main() {
     // Create a Pstring
     Pstring pstr;
-    pstr.length = 14;
-    snprintf(pstr.str, sizeof(pstr.str), "Hello Worldaaa");
+    pstr.length = 13;
+    Pstring pstr2;
+    pstr2.length = 12;
+
+    snprintf(pstr.str, sizeof(pstr.str), "Hello  World!");
+    snprintf(pstr2.str, sizeof(pstr2.str), "ShallomOllam");
 
     // Print the original string
     printf("Original string: %s\n", pstr.str);
+    printf("Original string2: %s\n", pstr2.str);
 
+
+    // Call the pstrijcpy function
+    // pstr2 = *(pstrijcpy(&pstr, &pstr2, 5, 9));
+    pstrijcpy(&pstr, &pstr2, 7, 11);
     // Call the pstrlen function
     printf("Length of string: %d\n", pstrlen(&pstr));
     // Call the swapCase function
@@ -26,6 +37,8 @@ int main() {
 
     // Print the modified string
     printf("Modified string: %s\n", pstr.str);
+    printf("Modified string2: %s\n", pstr2.str);
+
 
     return 0;
 }
